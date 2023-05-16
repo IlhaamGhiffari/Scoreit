df0fe61fe246631e0a33d80b5f6362d0a8c9a4a4
Muhaammad Ilhaam Ghiffari <ilham211@mhs.unsyiah.ac.id>
2023-05-16 14:56:53 +0700

    feat:add voli and bulu tangkis mcr

---
+++ app/Models/voli.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class voli extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'home',
+        'away',
+        'score1',
+        'set1',
+        'score2',
+        'set2',
+        'user_id',
+    ];
+
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+}
