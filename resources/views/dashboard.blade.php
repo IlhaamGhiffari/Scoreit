71976bf741dfda6d3f91b61b22ab4b4e3ab268fd
Haamergo <ilham211@mhs.unsyiah.ac.id>
2023-05-18 14:31:09 +0700

    yy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
             <div class="container">
               <p class="text-white text-[24px] max-w-[700px] text-justify">
               ScoreIt adalah sebuah aplikasi web yang menghadirkan papan skor digital untuk mencatat dan menampilkan catatan pertandingan dengan mudah. Dengan antarmuka yang intuitif, Anda dapat memasukkan skor, dan mencatat hasil pertandingan. Aplikasi ini tentunya berbeda dengan papan skor fisik dimana aplikisi ini memberikan kemudahan  dalam mencatat informasi hasil pertandingan, serta menampilkannya dalam sebuah tabel, menjadikan pengalaman pertandingan lebih modern dan interaktif.
-              Aplikisi Mempunyai fitur stopwatch dan 5 olahraga yaitu, Sepak Bola, Futsal, Basket, Voli, dan Bulutangkis
+              Aplikasi ini Mempunyai fitur stopwatch dan 5 olahraga yaitu, Sepak Bola, Futsal, Basket, Voli, dan Bulutangkis
               </p>
             </div>
             <div class="mt-[50px] grid grid-cols-3 gap-10">
